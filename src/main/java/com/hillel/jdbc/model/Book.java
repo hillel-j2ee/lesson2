d0a76f0c1e11ce5c4991a4b2cf92bef76542a7b6
dimaskomarov <komdi1999@gmail.com>
2020-10-25 01:50:26 +0300

    Class Book was changed. Genre type was changed(List -> String).

--- src/main/java/com/hillel/jdbc/model/Book.java
+++ src/main/java/com/hillel/jdbc/model/Book.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Book {
     private Long id;
     private String author;
-    private List<String> genre;
+    private String genre;
 
     public Long getId() {
         return id;
@@ -23,23 +23,16 @@ public class Book {
         this.author = author;
     }
 
-    public List<String> getGenre() {
+    public String getGenre() {
         return genre;
     }
 
-    public void setGenre(List<String> genre) {
+    public void setGenre(String genre) {
         this.genre = genre;
     }
 
     @Override
     public String toString() {
-        return String.format("Book{%d, %s, %s}", id, author, printList(genre));
-    }
-
-    private String printList(List<String> list) {
-        StringBuilder sb = new StringBuilder("{");
-        list.stream().forEach(el -> sb.append(el).append(","));
-        sb.deleteCharAt(sb.length() - 1);
-        return sb.append("}").toString();
+        return String.format("Book{%d, %s, %s}", id, author, genre);
     }
 }
